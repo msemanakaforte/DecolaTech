f9e36e7defdb2e6ca602bcfa65a4bb91bcd26dbd
Milena Semanaka <m.semanaka.forte@avanade.com>
2024-12-19 16:06:55 -0300

    App01

--- 01_Javascript/app01.js
+++ 01_Javascript/app01.js
@@ -1,2 +1,24 @@
-console.log("Hello World!");
+//declarando variáveis
+var curso = 'Javascript'; //visibilidade global
+let nome = 'Thais Santos Hello World!'; //visibilidade bloco
+let ch = 186;
+let st = true;
+let verifica = ch < 100;
 
+let lista = ['Java', 'Oracle', 'Angular', 100];
+let empresa = {
+    descricao: 'Avanade',
+    cidade: 'Recife',
+    ano: 2024,
+    funcionarios: ['Ana', 'Miguel', 'Jeferson']
+};
+let empresaJson = JSON.stringify(empresa);
+
+console.log(nome, '-', typeof (nome));
+console.log(curso, '-', typeof (curso));
+console.log(ch, '-', typeof (ch));
+console.log(st, '-', typeof (st));
+console.log(verifica, '-', typeof (verifica));
+console.log(lista, '-', typeof (lista));
+console.log(empresa, '-', typeof (empresa));
+console.log(empresaJson);

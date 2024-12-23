aaef757fd10f064db7ddfc1522a443827621315a
Milena Semanaka <m.semanaka.forte@avanade.com>
2024-12-23 14:07:35 -0300

    Função Funcionário

---
+++ 01_Javascript/classes/funcionario_fn.js
@@ -0,0 +1,24 @@
+// exemplo de como seria uma classe escrita como função
+
+function Funcionario(nome = 'Desconhecido', idade = 18, cargo = 'Desconhecido', salario = 2000) {
+    
+    if (nome == undefined || idade == undefined || cargo == undefined || salario == undefined) {
+        throw new Error('Dados incorretos!');
+    }
+    this.atribuir(nome, idade, cargo, salario); //chamou o método
+
+    // definiu o que ele é
+    this.atribuir = function(nome, idade, cargo, salario) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+
+    this.mostrarDados = function() {
+        return `Nome: ${this.nome}\nIdade: ${this.idade}\n` +
+            `Cargo: ${this.cargo}\nSalário: ${this.salario}`;
+    }
+}
+
+module.exports = Funcionario;
